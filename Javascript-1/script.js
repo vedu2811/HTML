654fe29dee1a5b6eb22b694be7eb32b997743a24
vedantjalan <vedant28j@gmail.com>
2024-07-18 00:16:19 +0530

    Values and Variables

--- Javascript-1/script.js
+++ Javascript-1/script.js
@@ -5,6 +5,21 @@ console.log("Vedant");
 console.log(23);
 
 let firstName = "Apple";
+
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
+let vedant_jalan = "VJ";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
