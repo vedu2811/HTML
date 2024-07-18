a812eff45ac2b0e0028be5d6ff0c39727dbe8650
vedantjalan <vedant28j@gmail.com>
2024-07-18 13:27:02 +0530

    Dynamic Typing

--- Javascript-1/script.js
+++ Javascript-1/script.js
@@ -30,7 +30,10 @@ console.log(myFirstJob);
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
-console.log(typeof true);
+// console.log(typeof true);
 console.log(typeof javascriptIsFun);
-console.log(typeof 23);
-console.log(typeof "vedant");
+// console.log(typeof 23);
+// console.log(typeof "vedant");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
