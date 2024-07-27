468b5de0fdd79d6fd99151ffc58aaed8c7042cff
vedantjalan <vedant28j@gmail.com>
2024-07-27 15:44:35 +0530

    Strings better

--- Javascript-1/script.js
+++ Javascript-1/script.js
@@ -111,3 +111,6 @@ const year = 2037;
 const jonas =
   "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
 console.log(jonas);
+
+const jonasNew = `I'm ${firstName}`;
+console.log(jonasNew);
