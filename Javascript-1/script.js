d80d00fa2904eb30fbc067b03544db02098a709e
vedantjalan <vedant28j@gmail.com>
2024-07-27 16:25:29 +0530

    Type Conversion

--- Javascript-1/script.js
+++ Javascript-1/script.js
@@ -121,7 +121,6 @@ console.log("String with \nmultiple \nlines");
 console.log(`string with
 multiple 
 lines`);
-*/
 
 const age = 15;
 
@@ -140,3 +139,17 @@ if (birthYear <= 2000) {
   century = 21;
 }
 console.log(century);
+*/
+
+// Type Conversion
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+console.log(inputYear + 18);
+
+console.log(Number("Vedant"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+//Type Coercion
