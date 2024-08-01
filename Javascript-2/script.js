ecfd40fc307828e32a137e3a879cff7ed99a5cf2
vedantjalan <vedant28j@gmail.com>
2024-08-02 01:15:09 +0530

    Functions

--- Javascript-2/script.js
+++ Javascript-2/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -8,3 +9,26 @@ if (hasDriversLicense) console.log("I can drive");
 
 // const interface = "Audio";
 // const private = 534;
+*/
+
+function logger() {
+  console.log("I'm vedant");
+}
+
+// calling/running/invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
