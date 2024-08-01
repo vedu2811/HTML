ea2bc9ea7a2f87016681c86051cf461635bad46c
vedantjalan <vedant28j@gmail.com>
2024-08-01 22:17:39 +0530

    Same code using if

--- Javascript-1/script.js
+++ Javascript-1/script.js
@@ -253,3 +253,18 @@ switch (day) {
   default:
     console.log("Not a valid day");
 }
+
+if (day === "monday") {
+  console.log("Plan course structure");
+  console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+  console.log("prepare videos");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("Write code examples");
+} else if (day === "friday") {
+  console.log("record videos");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("Enjoy weekend");
+} else {
+  console.log("Not a valid day");
+}
