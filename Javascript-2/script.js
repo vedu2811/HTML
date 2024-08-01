954d1d29772ef45af14c3cc4c6365c11df7de677
vedantjalan <vedant28j@gmail.com>
2024-08-02 01:27:39 +0530

    Function declaration & Function Expression

--- Javascript-2/script.js
+++ Javascript-2/script.js
@@ -9,7 +9,6 @@ if (hasDriversLicense) console.log("I can drive");
 
 // const interface = "Audio";
 // const private = 534;
-*/
 
 function logger() {
   console.log("I'm vedant");
@@ -32,3 +31,18 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number("23");
+*/
+
+//FUNCTION DECLARATION
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+
+//FUNCTION EXPRESSION
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
