bcc13af7e05bb67133891f53a9a7ef9dc601efb0
vedantjalan <vedant28j@gmail.com>
2024-08-02 15:02:09 +0530

    Arrow Function

--- Javascript-2/script.js
+++ Javascript-2/script.js
@@ -31,7 +31,6 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number("23");
-*/
 
 //FUNCTION DECLARATION
 function calcAge1(birthYear) {
@@ -46,3 +45,19 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
+*/
+
+//ARROW FUNCTION
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstNane) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstNane} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1991, "vedant"));
+console.log(yearsUntilRetirement(1980, "bob"));
