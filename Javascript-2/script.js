79a4312a19df2b054c093b50f052635799379648
vedantjalan <vedant28j@gmail.com>
2024-08-04 18:25:37 +0530

    Dot and bracket notations in objects

--- Javascript-2/script.js
+++ Javascript-2/script.js
@@ -185,3 +185,23 @@ const jonas = {
   job: "teacher",
   friends: ["Michael", "Peter", "Steven"],
 };
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas["lastName"]);
+
+const nameKey = "Name";
+console.log(jonas["first" + nameKey]);
+console.log(jonas["last" + nameKey]);
+
+// console.log(jonas.'last'+nameKey);
+
+const interestedIn = prompt(
+  "What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends"
+);
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log("Wrong request");
+}
