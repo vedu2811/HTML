b63277456db1ff7da9c3bd2f2f207641ffac1619
vedantjalan <vedant28j@gmail.com>
2024-08-16 00:22:34 +0530

    Debugging

--- DevSkills/script.js
+++ DevSkills/script.js
@@ -1,6 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
+/*
 //PROBLEM:
 // We work for a company building a smart home thermometer. Our most recent task to this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
@@ -60,3 +61,26 @@ const calcTempAmplitudeNew = function (t1, t2) {
 };
 const ampNew = calcTempAmplitudeNew([4, 5, 6, 2], [1, 2, -9, 0]);
 console.log(ampNew);
+*/
+
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celcius",
+
+    // C) FIX
+    value: Number(prompt("Degree celcius")),
+  };
+
+  // B) Find the Bug
+  console.table(measurement);
+
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) Identify the bug.
+console.log(measureKelvin());
