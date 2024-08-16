19d35bddb782c64d02eacd75461baeec04f2b07c
vedantjalan <vedant28j@gmail.com>
2024-08-16 22:54:43 +0530

    Handling the click events

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/*
 console.log(document.querySelector(".message").textContent);
 document.querySelector(".message").textContent = "🎉 Correct Number!";
 
@@ -8,3 +9,17 @@ document.querySelector(".score").textContent = 10;
 
 document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value);
+*/
+
+const x = function () {
+  console.log(23);
+};
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔ No Number!";
+  }
+});
