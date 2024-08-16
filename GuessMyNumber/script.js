87f44331a9d3cff7bea3d57f04660a377d6cbc16
vedantjalan <vedant28j@gmail.com>
2024-08-17 00:01:47 +0530

    Changing CSS STYLES in Javascript

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -13,16 +13,25 @@ console.log(document.querySelector(".guess").value);
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
+  //When there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "⛔ No Number!";
+
+    //When player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Guess";
+    document.querySelector(".number").textContent = secretNumber;
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+
+    document.querySelector(".number").style.width = "30rem";
+
+    //When guess is lower
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 Guess Bigger!";
@@ -32,6 +41,8 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "☹️ You Lost the Game";
       document.querySelector(".score").textContent = 0;
     }
+
+    //When guess is higher
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📉 Guess Lower!";
