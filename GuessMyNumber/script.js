5271c7edc12c03e46b1c43fac079970256c7c6ff
vedantjalan <vedant28j@gmail.com>
2024-08-16 23:48:59 +0530

    Handling Score

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -12,6 +12,7 @@ console.log(document.querySelector(".guess").value);
 */
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
@@ -24,7 +25,11 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "🎉 Correct Guess";
   } else if (guess < secretNumber) {
     document.querySelector(".message").textContent = "📈 Guess Bigger!";
+    score--;
+    document.querySelector(".score").textContent = score;
   } else if (guess > secretNumber) {
     document.querySelector(".message").textContent = "📉 Guess Lower!";
+    score--;
+    document.querySelector(".score").textContent = score;
   }
 });
