146ef44964aa953c98ebd99ca3c850a10883d11a
vedantjalan <vedant28j@gmail.com>
2024-08-17 22:56:41 +0530

    Switching active player - Efficient

--- PigGame/script.js
+++ PigGame/script.js
@@ -17,7 +17,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling Dice Functionality
 btnRoll.addEventListener("click", function () {
@@ -32,12 +34,15 @@ btnRoll.addEventListener("click", function () {
   if (dice !== 1) {
     //Current score updation
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //switch
-    score0El.textContent = current0El.textContent;
-    current0El.textContent = 0;
-    player0El.classList.remove("player--active");
-    player1El.classList.add("player--active");
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });
