a083b20f14e5b1015a89a0c14def9d758b74d7b8
vedantjalan <vedant28j@gmail.com>
2024-08-17 15:42:54 +0530

    Enabled closing the modal window through overlay added a function for closeModal to make the code DRY

--- Modal/script.js
+++ Modal/script.js
@@ -14,7 +14,10 @@ for (let i = 0; i < btnOpenModal.length; i++) {
   });
 }
 
-btnCloseModal.addEventListener("click", function () {
+const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
-});
+};
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
