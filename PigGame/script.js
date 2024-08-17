746584300ca62cc86b89e50214826dcea76b1170
vedantjalan <vedant28j@gmail.com>
2024-08-17 22:19:33 +0530

    Rolling Dice - 1

--- PigGame/script.js
+++ PigGame/script.js
@@ -3,9 +3,28 @@
 // selecting elements
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
+
+let currentScore = 0;
+
+// Rolling Dice Functionality
+btnRoll.addEventListener("click", function () {
+  // 1. Generating Random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check rolled is 1? if true, switch..
+});
