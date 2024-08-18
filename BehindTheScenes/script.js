5a32ea941ff666233e4dff9c297dd12380d8f0c6
vedantjalan <vedant28j@gmail.com>
2024-08-19 03:22:46 +0530

    Scoping concepts : var is accesible outside block, (function scoped)

--- BehindTheScenes/script.js
+++ BehindTheScenes/script.js
@@ -6,6 +6,14 @@ function calcAge(birthYear) {
   function printAge() {
     const output = `${firstName}, you are ${age}, born in ${birthYear}`;
     console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 2003) {
+      var millenial = true;
+      const str = `Oh, You're a millenial, ${firstName}`;
+      console.log(str);
+    }
+    // console.log(str); //Reference Error
+    console.log(millenial);
   }
   printAge();
 
