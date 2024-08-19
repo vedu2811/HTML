0e19afb8cc0319ac4722dc7246dc8877b8328c94
vedantjalan <vedant28j@gmail.com>
2024-08-19 20:57:50 +0530

    Arguments

--- BehindTheScenes/script.js
+++ BehindTheScenes/script.js
@@ -140,3 +140,10 @@ const Vedant = {
 };
 Vedant.greet();
 Vedant.calcAge();
+
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 3, 6);
