6797114764bb9cf024c5548af6b05107a4bd12de
vedantjalan <vedant28j@gmail.com>
2024-08-21 02:13:05 +0530

    Solution - Nullish assignment operator

--- DataStructures/script.js
+++ DataStructures/script.js
@@ -68,8 +68,12 @@ const rest2 = {
 // OR Assignment operator
 // rest1.numGuests = rest1.numGuests || 10;
 // rest2.numGuests = rest2.numGuests || 10;
-rest1.numGuests ||= 10;
-rest2.numGuests ||= 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// Nullish Assignment Operator
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
 
 console.log(rest1);
 console.log(rest2);
