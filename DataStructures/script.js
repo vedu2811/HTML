2ef81c3ccd62c6fd8dd699bc3e84572eae3538cb
vedantjalan <vedant28j@gmail.com>
2024-08-21 02:10:29 +0530

    OR Assignment Operator

--- DataStructures/script.js
+++ DataStructures/script.js
@@ -64,8 +64,11 @@ const rest2 = {
   owner: "abcd",
 };
 
-rest1.numGuests = rest1.numGuests || 10;
-rest2.numGuests = rest2.numGuests || 10;
+// OR Assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+rest1.numGuests ||= 10;
+rest2.numGuests ||= 10;
 
 console.log(rest1);
 console.log(rest2);
