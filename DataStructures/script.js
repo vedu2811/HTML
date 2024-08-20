d035dabb8f8a2ef1ffabe526972e32b6fb20408f
vedantjalan <vedant28j@gmail.com>
2024-08-21 01:57:46 +0530

    Nullish Coalescing Operator (??), it treates the 'null' and 'undefined' as truthy values

--- DataStructures/script.js
+++ DataStructures/script.js
@@ -58,6 +58,10 @@ restaurant.numGuests = 0;
 const guests = restaurant.numGuests || 10;
 console.log(guests);
 
+// Nullish: null and undefined (NOT 0 or ' ')
+const guestsCorrect = restaurant.numGuests ?? 10;
+console.log(guestsCorrect);
+
 /*
 /////////////////////////////////////
 // SHORT CIRCUITING (&& and ||)
