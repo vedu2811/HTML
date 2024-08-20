a1df1e454fb1e9654ba1995e490a32e24d251960
vedantjalan <vedant28j@gmail.com>
2024-08-21 02:14:57 +0530

    And operator

--- DataStructures/script.js
+++ DataStructures/script.js
@@ -75,6 +75,9 @@ const rest2 = {
 rest1.numGuests ??= 10;
 rest2.numGuests ??= 10;
 
+rest1.owner = rest1.owner && "anonymous";
+rest2.owner = rest2.owner && "anonymous";
+
 console.log(rest1);
 console.log(rest2);
 
