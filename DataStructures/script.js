3cd64f426795be9a92158a091b9001261774b3a2
vedantjalan <vedant28j@gmail.com>
2024-08-20 22:46:06 +0530

    Giving different names to the object methods

--- DataStructures/script.js
+++ DataStructures/script.js
@@ -35,6 +35,12 @@ const restaurant = {
 const { name, openingHours, categories } = restaurant;
 console.log(name, openingHours, categories);
 
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
 /*
 //////////////////////////////////
 DESTUCTURING ARRAYS
