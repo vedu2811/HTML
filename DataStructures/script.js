4bbf67bf0bf9f9273fb10f156f51b2428fe627c4
vedantjalan <vedant28j@gmail.com>
2024-08-22 21:58:20 +0530

    Optional Chaining (?)

--- DataStructures/script.js
+++ DataStructures/script.js
@@ -57,6 +57,12 @@ const restaurant = {
 if (restaurant.openingHours && restaurant.openingHours.mon)
   console.log(restaurant.openingHours.mon.open);
 
+// console.log(restaurant.openingHours.mon.open);
+
+//With Optional Chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
 /*
 ///////////////////////////////
 // FOR-OF loop
