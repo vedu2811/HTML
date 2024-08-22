ce2769fa8daef4f5f88b4e533746aab2df86a432
vedantjalan <vedant28j@gmail.com>
2024-08-22 21:42:29 +0530

    Better way of using for-of loop using destructuring

--- DataStructures/script.js
+++ DataStructures/script.js
@@ -58,10 +58,10 @@ const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 for (const item of menu) console.log(item);
 
-for (const item of menu.entries()) {
-  console.log(item);
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
 }
-console.log([...menu.entries()]);
+// console.log([...menu.entries()]);
 
 /*
 ////////////////////////////////
