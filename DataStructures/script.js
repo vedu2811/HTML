8d0561f3546c92aec33923bdff9e1285b31b389b
vedantjalan <vedant28j@gmail.com>
2024-08-22 21:52:01 +0530

    ES6 enhanced object literal - 3, new ways of using arrays elements in the object methods

--- DataStructures/script.js
+++ DataStructures/script.js
@@ -4,16 +4,18 @@
 const flights =
   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
+const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
 const openingHours = {
-  thu: {
+  [weekdays[3]]: {
     open: 12,
     close: 22,
   },
-  fri: {
+  [weekdays[4]]: {
     open: 11,
     close: 23,
   },
-  sat: {
+  [weekdays[5]]: {
     open: 0, // Open 24 hours
     close: 24,
   },
