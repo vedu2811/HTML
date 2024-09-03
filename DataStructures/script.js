564758b3f3a26c1d7e46a47dcf6365d60d29bd70
vedantjalan <vedant28j@gmail.com>
2024-09-04 00:26:16 +0530

    Coding challenge 1

--- DataStructures/script.js
+++ DataStructures/script.js
@@ -53,6 +53,29 @@ const restaurant = {
     console.log(otherIng);
   },
 };
+
+////////////////////////////////
+// Coding challenge 3
+////////////////////////////////
+
+const gameEvents = new Map([
+  [17, "⚽️ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽️ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽️ GOAL"],
+  [80, "⚽️ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+// 1.
+const arr = [...new Set(gameEvents.values())];
+console.log(arr);
+
 /*
 ///////////////////////////////////////
 // Maps: Iteration
