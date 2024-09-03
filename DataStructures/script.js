029f649faf8b1e87967a6de2878e6c7456ab4462
vedantjalan <vedant28j@gmail.com>
2024-09-03 23:23:40 +0530

    Introduction to Maps

--- DataStructures/script.js
+++ DataStructures/script.js
@@ -54,6 +54,20 @@ const restaurant = {
   },
 };
 
+const rest = new Map();
+rest.set("name", "Classico italiano");
+rest.set(1, "Firenze, Italy");
+console.log(rest.set(2, "Lisbon, Portugal"));
+
+rest
+  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+  .set("open", 11)
+  .set("close", 23)
+  .set(true, "We are open :D")
+  .set(false, "We are closed :(");
+
+// console.log(rest);
+
 /*
 ////////////////////////////////
 // Sets
