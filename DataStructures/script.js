7f3c0a059c198fd57b305c11ca1c51721e3dd9d7
vedantjalan <vedant28j@gmail.com>
2024-09-03 23:36:42 +0530

    Better way to construct maps

--- DataStructures/script.js
+++ DataStructures/script.js
@@ -54,6 +54,17 @@ const restaurant = {
   },
 };
 
+const question = new Map([
+  ["question", "What is the best prog language in the world?"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "JavaScript"],
+  ["correct", 3],
+  [true, "Correct"],
+  [false, "Try Again"],
+]);
+console.log(question);
+
 /*
 ////////////////////////////////////
 // Maps: Fundamentals
