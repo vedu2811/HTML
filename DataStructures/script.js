b889296cd29ece2036e5e7e0cf6fbf3c4e8e96c3
vedantjalan <vedant28j@gmail.com>
2024-09-05 00:06:09 +0530

    Capitalize names method 2
    "

--- DataStructures/script.js
+++ DataStructures/script.js
@@ -67,7 +67,8 @@ const capitalizeName = function (name) {
   const namesUpper = [];
 
   for (const n of names) {
-    namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
   }
   console.log(namesUpper.join(" "));
 };
