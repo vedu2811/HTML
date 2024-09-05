dc7f0c0ff172aa3f5376fb3f1d18b9992b0c22c5
vedantjalan <vedant28j@gmail.com>
2024-09-06 03:11:03 +0530

    Coding challenge 1 starter code

--- Functions/script.js
+++ Functions/script.js
@@ -199,3 +199,10 @@ const addVAT1 = addTax1(0.23);
 console.log(addVAT1(100));
 console.log(addVAT1(23));
 */
+
+// Coding Challenge #1
+const poll = {
+  question: "What is your favorite programming language?",
+  options: ["0: Javascript", "1: Python", "2: Rust", "3: C++"],
+  answers: new Array(4).fill(0),
+};
