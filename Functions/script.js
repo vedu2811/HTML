84eee3fc022898caec744cc044c5d66e36c5615a
vedantjalan <vedant28j@gmail.com>
2024-09-06 02:58:16 +0530

    Function returning a function

--- Functions/script.js
+++ Functions/script.js
@@ -188,3 +188,14 @@ const addVAT = addTax.bind(null, 0.23);
 
 console.log(addVAT(100));
 console.log(addVAT(23));
+
+// Function returning Function
+const addTax1 = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT1 = addTax1(0.23);
+console.log(addVAT1(100));
+console.log(addVAT1(23));
