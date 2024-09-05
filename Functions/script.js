2b8300d4e7d1c11cdc6ef2f1499fbf516c5a7568
vedantjalan <vedant28j@gmail.com>
2024-09-05 23:47:46 +0530

    Create Booking function with undefined parameters

--- Functions/script.js
+++ Functions/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassenger, price) {
+  const booking = {
+    flightNum,
+    numPassenger,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("LH123");
