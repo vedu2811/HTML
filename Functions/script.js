06f935f44bd33b3c44238eee0e3709fcd828111d
vedantjalan <vedant28j@gmail.com>
2024-09-06 00:50:43 +0530

    Higher Order Function

--- Functions/script.js
+++ Functions/script.js
@@ -64,5 +64,15 @@ const oneWord = function (str) {
 
 const upperFirstWord = function (str) {
   const [first, ...others] = str.split(" ");
-  return [first.touppercase(), ...others].join(" ");
+  return [first.toUpperCase(), ...others].join(" ");
 };
+
+// Higher Order Function
+const transformer = function (str, fn) {
+  console.log(`Original String: ${str}`);
+  console.log(`Transformed String: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer("JavaScript is the best!", upperFirstWord);
