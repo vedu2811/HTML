473b46cd5c8e31bdd8b943652d7f2508d2b74bde
vedantjalan <vedant28j@gmail.com>
2024-09-05 23:48:08 +0530

    Defining default parameters in old way

--- Functions/script.js
+++ Functions/script.js
@@ -3,6 +3,9 @@
 const bookings = [];
 
 const createBooking = function (flightNum, numPassenger, price) {
+  numPassenger = numPassenger || 1;
+  price = price || 199;
+
   const booking = {
     flightNum,
     numPassenger,
