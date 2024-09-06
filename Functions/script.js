bbf5d652d25ad84f91ea7cf69c24528b999f2cea
vedantjalan <vedant28j@gmail.com>
2024-09-07 01:10:49 +0530

    Re-assigning f function

--- Functions/script.js
+++ Functions/script.js
@@ -290,5 +290,17 @@ const g = function () {
   };
 };
 
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
 g();
 f();
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
