669dd8d219f8e411bdddfa6f73449ea3f91aca69
vedantjalan <vedant28j@gmail.com>
2024-09-08 01:24:42 +0530

    Accessing for Of loop

--- Arrays/script.js
+++ Arrays/script.js
@@ -119,11 +119,11 @@ console.log("jonas".at(-1));
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
   if (movement > 0) {
-    console.log(`You deposited ${movement}`);
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
   } else {
-    console.log(`You withdrew ${Math.abs(movement)}`);
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
   }
 }
 
