060ca22101177e0d17023a53e43d537a40821d61
vedantjalan <vedant28j@gmail.com>
2024-09-07 21:16:23 +0530

    Array Methods - SLICE

--- Arrays/script.js
+++ Arrays/script.js
@@ -74,3 +74,13 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ["a", "b", "c", "d", "e"];
+
+// SLICE
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
