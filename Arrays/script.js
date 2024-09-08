62810821c36249e046e76df2bfd20f0d3c9666e8
vedantjalan <vedant28j@gmail.com>
2024-09-09 02:34:21 +0530

    Same function using forOf loop

--- Arrays/script.js
+++ Arrays/script.js
@@ -272,5 +272,11 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const balance = movements.reduce(function (acc, cur, i, arr) {
   console.log(`Iteration ${i}: ${acc}`);
   return acc + cur;
-}, 100);
+}, 0);
 console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) {
+  balance2 += mov;
+}
+console.log(balance2);
