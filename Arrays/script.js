0cb8aec339722e4e8f710b3b1b22549156b4e96a
vedantjalan <vedant28j@gmail.com>
2024-09-09 02:10:46 +0530

    Chaning the pin for the different usernames

--- Arrays/script.js
+++ Arrays/script.js
@@ -9,28 +9,28 @@ const account1 = {
   owner: "Vedant Jalan",
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
-  pin: 1111,
+  pin: 9182,
 };
 
 const account2 = {
   owner: "Abir Chodha",
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
-  pin: 2222,
+  pin: 8727,
 };
 
 const account3 = {
   owner: "Surya Pratap Singh",
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
-  pin: 3333,
+  pin: 7266,
 };
 
 const account4 = {
   owner: "Uday Kishor Pawar",
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
-  pin: 4444,
+  pin: 8773,
 };
 
 const accounts = [account1, account2, account3, account4];
