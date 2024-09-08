757be7980626834eb6a711ca6d80725b49abc7fb
vedantjalan <vedant28j@gmail.com>
2024-09-09 02:32:02 +0530

    The REDUCE Method and its visualization

--- Arrays/script.js
+++ Arrays/script.js
@@ -266,3 +266,11 @@ console.log(withdrawalFor);
 */
 
 // The REDUCE Method
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// acc - Accumulator is like a snowball
+const balance = movements.reduce(function (acc, cur, i, arr) {
+  console.log(`Iteration ${i}: ${acc}`);
+  return acc + cur;
+}, 0);
+console.log(balance);
