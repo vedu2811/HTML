bb4f78b3715589b8792408a9fa6105a04d777eb6
vedantjalan <vedant28j@gmail.com>
2024-09-09 02:54:31 +0530

    Adding the calculated balance in the main site using appropriate DOM

--- Arrays/script.js
+++ Arrays/script.js
@@ -81,6 +81,12 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, cur) => acc + cur, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayBalance(account1.movements);
+
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
     acc.userName = acc.owner
