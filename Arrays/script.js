7b23f5430f6e8049dc4274b634fc4ed5c3249f3a
vedantjalan <vedant28j@gmail.com>
2024-09-09 02:15:08 +0530

    The FILTER Method

--- Arrays/script.js
+++ Arrays/script.js
@@ -237,3 +237,11 @@ const movementsDescriptive = movements.map(function (mov, i, arr) {
 });
 console.log(movementsDescriptive);
 */
+
+// FILTER METHOD
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(deposits);
