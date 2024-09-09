8406ed11feb9cd2c882069954df02354effe94c7
vedantjalan <vedant28j@gmail.com>
2024-09-09 14:53:01 +0530

    Chaining all the methods in one statement

--- Arrays/script.js
+++ Arrays/script.js
@@ -314,3 +314,14 @@ const calcAverageHumanAge = function (ages) {
 };
 calcAverageHumanAge(ages);
 */
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const euroToUsd = 1.1;
+
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter((mov) => mov > 0)
+  .map((mov) => mov * euroToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
