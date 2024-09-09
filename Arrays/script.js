00e4d00b607ef4783435683afc017b0472897bf3
vedantjalan <vedant28j@gmail.com>
2024-09-09 16:30:44 +0530

    find method in the accounts array

--- Arrays/script.js
+++ Arrays/script.js
@@ -365,3 +365,8 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const firstWithdrawal = movements.find((mov) => mov < 0);
 console.log(firstWithdrawal);
+
+console.log(accounts);
+
+const account = accounts.find((acc) => (acc.owner = "Vedant Jalan"));
+console.log(account);
