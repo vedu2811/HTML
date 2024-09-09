80ac57d973d3c4650d13c86791245702bc1d50f8
vedantjalan <vedant28j@gmail.com>
2024-09-10 01:11:48 +0530

    Sort method does not work on numbers - it only works on strings

--- Arrays/script.js
+++ Arrays/script.js
@@ -530,3 +530,8 @@ console.log(overallBalance2);
 const owners = ["Vedant", "Abir", "Surya", "Uday"];
 console.log(owners.sort());
 console.log(owners);
+
+// Numbers
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+console.log(movements);
+console.log(movements.sort());
