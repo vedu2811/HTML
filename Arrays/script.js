4b52d24211c9e1c7c7f40116cc97b977199f2f6b
vedantjalan <vedant28j@gmail.com>
2024-09-10 01:02:22 +0530

    Joining all the account's movement arrays to one array of arrays

--- Arrays/script.js
+++ Arrays/script.js
@@ -506,3 +506,6 @@ console.log(arr.flat());
 
 const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
 console.log(arrDeep.flat(2));
+
+const accountMovement = accounts.map((acc) => acc.movements);
+console.log(accountMovement);
