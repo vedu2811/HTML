4e631500612d9c6e29878b756cc0fe41f748dc80
vedantjalan <vedant28j@gmail.com>
2024-09-10 01:30:09 +0530

    Sorting - 1 - Adding the sorting condition and functionality in the displayMovements function

--- Arrays/script.js
+++ Arrays/script.js
@@ -61,11 +61,13 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = "";
   // .textContent=0;
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
 
     const html = `
