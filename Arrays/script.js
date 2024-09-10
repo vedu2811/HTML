037f88a62d1945efdc9ec45e77b4a9b7b99fe6ee
vedantjalan <vedant28j@gmail.com>
2024-09-10 14:29:44 +0530

    Array with 7 empty values

--- Arrays/script.js
+++ Arrays/script.js
@@ -565,3 +565,7 @@ console.log(movements);
 
 const arr = [1, 2, 3, 4, 5, 6, 7];
 console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty Array + fill method
+const x = new Array(7);
+console.log(x);
