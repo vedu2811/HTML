fae051f31e68c7742063b9751907f15310ba2708
vedantjalan <vedant28j@gmail.com>
2024-09-12 02:37:00 +0530

    Is Even function

--- Numbers/script.js
+++ Numbers/script.js
@@ -336,3 +336,10 @@ console.log(5 % 2);
 console.log(5 / 2);
 
 console.log(8 % 3);
+console.log(6 % 2);
+console.log(7 % 2);
+
+const isEven = (n) => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(12));
+console.log(isEven(9));
