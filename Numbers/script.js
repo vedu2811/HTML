9599c9948489259f6d94927ebbdc3efeabfb7f7f
vedantjalan <vedant28j@gmail.com>
2024-09-11 15:34:59 +0530

    Parse Float

--- Numbers/script.js
+++ Numbers/script.js
@@ -272,3 +272,6 @@ console.log(+"23");
 // Parsing
 console.log(Number.parseInt("30px", 10));
 console.log(Number.parseInt("e30", 10));
+
+console.log(Number.parseInt("   2.5rem  "));
+console.log(Number.parseFloat("   2.5rem  "));
