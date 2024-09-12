f1b9cbdae2ee98c2944133ab6de2b26e7f5da243
vedantjalan <vedant28j@gmail.com>
2024-09-12 15:55:35 +0530

    Writing current date on bankist in the correct format

--- Numbers/script.js
+++ Numbers/script.js
@@ -160,6 +160,16 @@ currentAccount = account1;
 updateUI(currentAccount);
 containerApp.style.opacity = 100;
 
+const now = new Date();
+const day = now.getDate();
+const month = now.getMonth() + 1;
+const year = now.getFullYear();
+const hour = now.getHours();
+const min = now.getMinutes();
+labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+
+// day/month/year
+
 btnLogin.addEventListener("click", function (e) {
   // Prevent form from submitting
   e.preventDefault();
