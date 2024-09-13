f0326cbd737c81f925b88078ff75302aa98c5565
vedantjalan <vedant28j@gmail.com>
2024-09-13 11:45:34 +0530

    Unit of the numbers

--- Numbers/script.js
+++ Numbers/script.js
@@ -519,12 +519,17 @@ console.log(days1);
 
 const num = 2234534.43;
 
-console.log("US: ", new Intl.NumberFormat("en-US").format(num));
-console.log("Germany: ", new Intl.NumberFormat("de-DE").format(num));
-console.log("Syria: ", new Intl.NumberFormat("ar-SY").format(num));
-console.log("India: ", new Intl.NumberFormat("en-IN").format(num));
+const options = {
+  style: "unit",
+  unit: "mile-per-hour",
+};
+
+console.log("US: ", new Intl.NumberFormat("en-US", options).format(num));
+console.log("Germany: ", new Intl.NumberFormat("de-DE", options).format(num));
+console.log("Syria: ", new Intl.NumberFormat("ar-SY", options).format(num));
+console.log("India: ", new Intl.NumberFormat("en-IN", options).format(num));
 console.log(
   "Browser: ",
   navigator.language,
-  new Intl.NumberFormat(navigator.language).format(num)
+  new Intl.NumberFormat(navigator.language, options).format(num)
 );
