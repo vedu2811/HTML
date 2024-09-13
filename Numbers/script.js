9bcdc04e7117d34893251211267ca0c255cbe2b6
vedantjalan <vedant28j@gmail.com>
2024-09-13 09:55:19 +0530

    Operation with dates - calculating number of days between two dates

--- Numbers/script.js
+++ Numbers/script.js
@@ -475,3 +475,13 @@ console.log(Date.now());
 future.setFullYear(2040);
 console.log(future);
 */
+
+// Operations with DATES
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);
+
+const calcDaysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 4));
+console.log(days1);
