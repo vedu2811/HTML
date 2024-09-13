866f7c8558a2e1adbbba3d2d31ecb73c2e59e4a7
vedantjalan <vedant28j@gmail.com>
2024-09-13 10:07:59 +0530

    Operation on dates onbankist app

--- Numbers/script.js
+++ Numbers/script.js
@@ -88,6 +88,10 @@ const formatMovementDate = function (date) {
   const daysPassed = calcDaysPassed(new Date(), date);
   console.log(daysPassed);
 
+  if (daysPassed === 0) return "Today";
+  if (daysPassed === 1) return "Yesterday";
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+
   const day = `${date.getDate()}`.padStart(2, 0);
   const month = `${date.getMonth() + 1}`.padStart(2, 0);
   const year = date.getFullYear();
