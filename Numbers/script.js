526c3ec1d75086be7affadd2cb088639d846142c
vedantjalan <vedant28j@gmail.com>
2024-09-13 11:11:59 +0530

    Internatioanlizing numbers API

--- Numbers/script.js
+++ Numbers/script.js
@@ -516,3 +516,15 @@ const calcDaysPassed = (date1, date2) =>
 const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 4));
 console.log(days1);
 */
+
+const num = 2234534.43;
+
+console.log("US: ", new Intl.NumberFormat("en-US").format(num));
+console.log("Germany: ", new Intl.NumberFormat("de-DE").format(num));
+console.log("Syria: ", new Intl.NumberFormat("ar-SY").format(num));
+console.log("India: ", new Intl.NumberFormat("en-IN").format(num));
+console.log(
+  "Browser: ",
+  navigator.language,
+  new Intl.NumberFormat(navigator.language).format(num)
+);
