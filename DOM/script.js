10c803e66e39bce0dc45edfb4e1489fafd4f0cc9
vedantjalan <vedant28j@gmail.com>
2024-09-14 02:28:17 +0530

    getElementsByTagName - updates automatically by the changes going on in html

--- DOM/script.js
+++ DOM/script.js
@@ -41,3 +41,7 @@ console.log(document.body);
 document.querySelector(".header");
 const allSections = document.querySelectorAll(".section");
 console.log(allSections);
+
+document.getElementById("#section--1");
+const allButtons = document.getElementsByTagName("button");
+console.log(allButtons);
