593207c0bedc3a4e63c21d37f2fbeffe0194cec2
vedantjalan <vedant28j@gmail.com>
2024-09-14 16:44:37 +0530

    mouseenter event: event which occurs on hover of a mouse

--- DOM/script.js
+++ DOM/script.js
@@ -151,3 +151,9 @@ btnScrollTo.addEventListener("click", function (e) {
 
   section1.scrollIntoView({ behavior: "smooth" });
 });
+
+const h1 = document.querySelector("h1");
+
+h1.addEventListener("mouseenter", function (e) {
+  alert("addEventListener: Great! You are reading the heading :D");
+});
