f70f87c3d6135a174e426f32ec1110ed3f8e9a93
vedantjalan <vedant28j@gmail.com>
2024-09-14 16:30:01 +0530

    Adding smooth behavior scrolling

--- DOM/script.js
+++ DOM/script.js
@@ -138,8 +138,14 @@ btnScrollTo.addEventListener("click", function (e) {
   );
 
   // Scrolling
-  window.scrollTo(
-    s1coords.left + window.pageXOffset,
-    s1coords.top + window.pageYOffset
-  );
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: "smooth",
+  });
 });
