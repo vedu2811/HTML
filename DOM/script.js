e8c66b2ca012fbd017c9f28707339b971d4928dd
vedantjalan <vedant28j@gmail.com>
2024-09-14 15:58:54 +0530

    getBoundingClientRect method

--- DOM/script.js
+++ DOM/script.js
@@ -119,3 +119,13 @@ logo.classList.contains("c");
 // DONT USE EVER
 logo.className = "jonas";
 */
+
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+});
