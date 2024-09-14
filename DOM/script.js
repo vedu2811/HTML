1761f85a4e9f62598b7da3eb3881135db5d80666
vedantjalan <vedant28j@gmail.com>
2024-09-14 09:25:13 +0530

    Most suitable is: append

--- DOM/script.js
+++ DOM/script.js
@@ -56,6 +56,6 @@ message.classList.add("cookie-message");
 // message.textContent = `We use cookies for improved functionality and analytics.`;
 message.innerHTML = `We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>`;
 
-header.prepend(message);
-// header.append(message);
-header.append(message.cloneNode(true));
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
