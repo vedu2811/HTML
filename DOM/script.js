dbe399e495b33594a6eee4c7f858b8d4a4c2f729
vedantjalan <vedant28j@gmail.com>
2024-09-15 13:04:45 +0530

    Tabbed Component - 5: Active tabs completed

--- DOM/script.js
+++ DOM/script.js
@@ -104,6 +104,8 @@ tabsContainer.addEventListener("click", function (e) {
   // Guard Clause
   if (!clicked) return;
 
+  // Active Tab
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
   clicked.classList.add("operations__tab--active");
 });
 
