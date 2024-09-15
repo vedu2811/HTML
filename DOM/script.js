8b08810b77cce513328e04aa3af81c1acec1e256
vedantjalan <vedant28j@gmail.com>
2024-09-15 20:21:36 +0530

    Reveal Sections - 8: Unobserving the entry.target

--- DOM/script.js
+++ DOM/script.js
@@ -190,11 +190,12 @@ const allSections = document.querySelectorAll(".section");
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
 
   if (!entry.isIntersecting) return;
 
   entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
 };
 
 const sectionObserver = new IntersectionObserver(revealSection, {
