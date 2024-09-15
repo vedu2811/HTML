2c83ab9b25736adf3f3a46402206f5f56d01b39a
vedantjalan <vedant28j@gmail.com>
2024-09-15 15:58:34 +0530

    Menu Fade Animation - 5: Calling the handlehover function using arguments

--- DOM/script.js
+++ DOM/script.js
@@ -131,29 +131,11 @@ const handleHover = function (e, opacity) {
 };
 
 nav.addEventListener("mouseover", function (e) {
-  if (e.target.classList.contains("nav__link")) {
-    const link = e.target;
-    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
-    const logo = link.closest(".nav").querySelector("img");
-
-    siblings.forEach((el) => {
-      if (el !== link) el.style.opacity = 0.5;
-    });
-    logo.style.opacity = 0.5;
-  }
+  handleHover(e, 0.5);
 });
 
 nav.addEventListener("mouseout", function (e) {
-  if (e.target.classList.contains("nav__link")) {
-    const link = e.target;
-    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
-    const logo = link.closest(".nav").querySelector("img");
-
-    siblings.forEach((el) => {
-      if (el !== link) el.style.opacity = 1;
-    });
-    logo.style.opacity = 1;
-  }
+  handleHover(e, 1.0);
 });
 
 //////////////////////////////////////////
