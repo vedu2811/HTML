149dcfe53d65d9fa19121ca059e2c0e91e049184
vedantjalan <vedant28j@gmail.com>
2024-09-15 11:47:28 +0530

    the closest() methods finds the parents of a particular element

--- DOM/script.js
+++ DOM/script.js
@@ -231,3 +231,7 @@ h1.lastElementChild.style.color = "orangered";
 // GOING UPWARDS
 console.log(h1.parentNode);
 console.log(h1.parentElement);
+
+h1.closest(".header").style.background = "var(--gradient-secondary)";
+
+h1.closest("h1").style.background = "var(--gradient-primary)";
