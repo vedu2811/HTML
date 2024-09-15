2c14bc27634d6f073ebc441f72dde8345e2017ef
vedantjalan <vedant28j@gmail.com>
2024-09-16 02:23:33 +0530

    load event executed when the full page is executed completely

--- DOM/script.js
+++ DOM/script.js
@@ -483,3 +483,7 @@ console.log(h1.parentElement.children);
 document.addEventListener("DOMContentLoaded", function (e) {
   console.log("HTML parsed and DOM tree built!", e);
 });
+
+window.addEventListener("load", function (e) {
+  console.log("Page Fully Loaded", e);
+});
