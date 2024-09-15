5247f58d0ffe6cbc6645ea4b2546bb0403f07314
vedantjalan <vedant28j@gmail.com>
2024-09-15 19:30:49 +0530

    Sticky navigation using Intersection Observer API - 1

--- DOM/script.js
+++ DOM/script.js
@@ -151,6 +151,20 @@ window.addEventListener("scroll", function () {
 //////////////////////////////////////////////////////////
 // STICKY NAVIGATION USING INTERSECTION OBSERVER API
 
+const obsCallBack = function (entries, observer) {
+  entries.forEach((entry) => {
+    console.log(entry);
+  });
+};
+
+const obsOptions = {
+  root: null,
+  threshold: 0.1,
+};
+
+const observer = new IntersectionObserver(obsCallBack, obsOptions);
+observer.observe(section1);
+
 //////////////////////////////////////////
 //////////////////////////////////////////
 //////////////////////////////////////////
