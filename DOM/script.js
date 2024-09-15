d25626c5c89b1a7678dde8ade3ca4066ef6204dc
vedantjalan <vedant28j@gmail.com>
2024-09-15 11:55:42 +0530

    Comments

--- DOM/script.js
+++ DOM/script.js
@@ -216,7 +216,6 @@ document.querySelector(".nav").addEventListener("click", function (e) {
   this.style.backgroundColor = randomColor();
   console.log("NAV", e.target, e.currentTarget);
 });
-*/
 
 const h1 = document.querySelector("h1");
 
@@ -248,3 +247,4 @@ console.log(h1.parentElement.children);
 [...h1.parentElement.children].forEach(function (el) {
   if (el !== h1) el.style.transform = "scale(0.5)";
 });
+*/
