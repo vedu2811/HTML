2653f5d053758fc00b4009ea7eedcdf6a0d3bf73
vedantjalan <vedant28j@gmail.com>
2024-09-15 16:03:12 +0530

    Menu Fade Animation - 6: Optimized solution: using bind method and this keyword

--- DOM/script.js
+++ DOM/script.js
@@ -117,26 +117,22 @@ tabsContainer.addEventListener("click", function (e) {
 
 //////////////////////////////////////////
 // MENU FADE ANIMATION
-const handleHover = function (e, opacity) {
+const handleHover = function (e) {
   if (e.target.classList.contains("nav__link")) {
     const link = e.target;
     const siblings = link.closest(".nav").querySelectorAll(".nav__link");
     const logo = link.closest(".nav").querySelector("img");
 
     siblings.forEach((el) => {
-      if (el !== link) el.style.opacity = opacity;
+      if (el !== link) el.style.opacity = this;
     });
-    logo.style.opacity = opacity;
+    logo.style.opacity = this;
   }
 };
 
-nav.addEventListener("mouseover", function (e) {
-  handleHover(e, 0.5);
-});
+nav.addEventListener("mouseover", handleHover.bind(0.5));
 
-nav.addEventListener("mouseout", function (e) {
-  handleHover(e, 1.0);
-});
+nav.addEventListener("mouseout", handleHover.bind(1));
 
 //////////////////////////////////////////
 //////////////////////////////////////////
