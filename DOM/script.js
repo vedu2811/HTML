83524b9d875f909dbe61db31664718565e5614d7
vedantjalan <vedant28j@gmail.com>
2024-09-15 19:51:55 +0530

    Sticky navigation using Intersection Observer API - 6: adding a negative root margin so that the sticky nav gets applied some pixels before the end of the header

--- DOM/script.js
+++ DOM/script.js
@@ -178,6 +178,7 @@ const stickyNav = function (entries) {
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
+  rootMargin: "-90px",
 });
 headerObserver.observe(header);
 
