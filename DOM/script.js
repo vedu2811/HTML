a74d0c5b38c4bc715f468568582fa60617342d09
vedantjalan <vedant28j@gmail.com>
2024-09-15 16:32:00 +0530

    Applying sticky nav (poor method) never use scroll events

--- DOM/script.js
+++ DOM/script.js
@@ -135,6 +135,18 @@ nav.addEventListener("mouseover", handleHover.bind(0.5));
 nav.addEventListener("mouseout", handleHover.bind(1));
 
 //////////////////////////////////////////
+// STICKY NAVIGATION
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
+window.addEventListener("scroll", function () {
+  console.log(window.scrollY);
+
+  if (window.scrollY > initialCoords.top) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+});
+
+//////////////////////////////////////////
 //////////////////////////////////////////
 //////////////////////////////////////////
 /*
