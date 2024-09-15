ba46c004298e8ae857c9e2572e0ceb9bbebe4fdc
vedantjalan <vedant28j@gmail.com>
2024-09-15 19:48:52 +0530

    Sticky navigation using Intersection Observer API - 5: Adding and removing the sticky classes

--- DOM/script.js
+++ DOM/script.js
@@ -169,7 +169,10 @@ const header = document.querySelector(".header");
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
 };
 
 const headerObserver = new IntersectionObserver(stickyNav, {
