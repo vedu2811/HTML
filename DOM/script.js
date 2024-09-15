bd04790a3b2161c861015e66977a5e590fb1f757
vedantjalan <vedant28j@gmail.com>
2024-09-15 09:29:31 +0530

    Running the function only once

--- DOM/script.js
+++ DOM/script.js
@@ -154,10 +154,14 @@ btnScrollTo.addEventListener("click", function (e) {
 
 const h1 = document.querySelector("h1");
 
-h1.addEventListener("mouseenter", function (e) {
+const alertH1 = function (e) {
   alert("addEventListener: Great! You are reading the heading :D");
-});
 
-h1.onmouseenter = function (e) {
-  alert("onmouseenter: Great! You are reading the heading :D");
+  h1.removeEventListener("mouseenter", alertH1);
 };
+
+h1.addEventListener("mouseenter", alertH1);
+
+// h1.onmouseenter = function (e) {
+//   alert("onmouseenter: Great! You are reading the heading :D");
+// };
