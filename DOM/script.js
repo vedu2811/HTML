f795fde258e87268796271193969a0b2dd02a0c0
vedantjalan <vedant28j@gmail.com>
2024-09-15 10:11:19 +0530

    Bubbling concept - 4: Setting capturing event to true, this leads the capturing event to occur first then bubbling events

--- DOM/script.js
+++ DOM/script.js
@@ -177,7 +177,7 @@ document.querySelector(".nav__link").addEventListener("click", function (e) {
   console.log("LINK", e.target, e.currentTarget);
 
   // STOP PROPAGATION
-  e.stopPropagation();
+  // e.stopPropagation();
 });
 
 document.querySelector(".nav__links").addEventListener("click", function (e) {
@@ -185,7 +185,11 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
   console.log("CONTAINER", e.target, e.currentTarget);
 });
 
-document.querySelector(".nav").addEventListener("click", function (e) {
-  this.style.backgroundColor = randomColor();
-  console.log("NAV", e.target, e.currentTarget);
-});
+document.querySelector(".nav").addEventListener(
+  "click",
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log("NAV", e.target, e.currentTarget);
+  },
+  true
+);
