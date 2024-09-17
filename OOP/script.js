f8b25982f7c2851e866b67668d87e99aafa8b88c
vedantjalan <vedant28j@gmail.com>
2024-09-18 01:21:34 +0530

    Making more objects

--- OOP/script.js
+++ OOP/script.js
@@ -12,3 +12,7 @@ console.log(vedant);
 // 2. function is created, this={}
 // 3. {} linked to prototype
 // 4. function automatically return {}
+
+const matilda = new Person("Matilda", 2017);
+const jack = new Person("Jack", 2011);
+console.log(matilda, jack);
