9deac21fe207b5e76d938ac163c9cab49c2e95e5
vedantjalan <vedant28j@gmail.com>
2024-09-17 20:50:00 +0530

    Converting the function to the class declaration

--- OOP/script.js
+++ OOP/script.js
@@ -1,10 +1,12 @@
 "use strict";
 
 const Person = function (firstName, birthYear) {
-  console.log(this);
+  this.firstName = firstName;
+  this.birthYear = birthYear;
 };
 
-new Person("Vedant", 2003);
+const vedant = new Person("Vedant", 2003);
+console.log(vedant);
 
 // 1. New {} is created
 // 2. function is created, this={}
