2db3b1e5fe9baf888583336aabc101bc95fc6866
vedantjalan <vedant28j@gmail.com>
2024-09-17 20:47:59 +0530

    Background of the constructor function

--- OOP/script.js
+++ OOP/script.js
@@ -1,5 +1,12 @@
 "use strict";
 
-const Person = function (firstName, birthYear) {};
+const Person = function (firstName, birthYear) {
+  console.log(this);
+};
 
 new Person("Vedant", 2003);
+
+// 1. New {} is created
+// 2. function is created, this={}
+// 3. {} linked to prototype
+// 4. function automatically return {}
