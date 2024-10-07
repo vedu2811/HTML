890b561d8c2c4403108f17cd9206e7ac9c755910
vedantjalan <vedant28j@gmail.com>
2024-10-07 11:52:24 +0530

    Array Prototype

--- OOP/script.js
+++ OOP/script.js
@@ -57,3 +57,9 @@ console.log(vedant.__proto__.__proto__);
 console.log(vedant.__proto__.__proto__.__proto__); //NULL
 
 console.dir(Person.prototype.constructor);
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
