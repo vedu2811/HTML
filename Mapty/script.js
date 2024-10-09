a62507628cfd8d81872170b924228d9eb01a072f
vedantjalan <vedant28j@gmail.com>
2024-10-09 16:01:53 +0530

    Navigator geolocation API

--- Mapty/script.js
+++ Mapty/script.js
@@ -10,3 +10,12 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    console.log(position);
+  },
+  function () {
+    alert("Could not get your position");
+  }
+);
