6c72cc7d8ecba8617fa30e50ef901b2a17ac75fc
vedantjalan <vedant28j@gmail.com>
2024-10-09 11:00:28 +0530

    Private Class Fields (uses #)

--- OOP/script.js
+++ OOP/script.js
@@ -421,8 +421,10 @@ vedant.calcAge();
 
 class Account {
   // Public Fields
-  _movements = [];
   locale = navigator.language;
+
+  // Private Fields
+  #movements = [];
 
   constructor(owner, currency, pin) {
     this.owner = owner;
@@ -437,11 +439,11 @@ class Account {
 
   // Public Interface
   getMovements() {
-    return this._movements;
+    return this.#movements;
   }
 
   deposit(val) {
-    this._movements.push(val);
+    this.#movements.push(val);
   }
 
   withdraw(val) {
@@ -471,3 +473,5 @@ console.log(acc1.getMovements());
 
 console.log(acc1);
 // console.log(acc1.pin);
+
+// console.log(acc1.#movements);
